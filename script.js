61b3e732c8bc5563a5ddf833ef0b0af66c70a26a
Agus27111 <agussetiawan.belajar@gmail.com>
2023-11-25 21:54:17 +0700

    styling but removeClass didnt Work yet

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ini untuk handle SOUND nya
 //buat fungsi playsound
 window.addEventListener("keydown", playSound);
 
@@ -9,4 +10,19 @@ function playSound(e) {
 
   audio.currentTime = 0; //reset waktu audio
   audio.play();
+
+  //ini untuk handle CSS nya
+  key.classList.add("playing");
 }
+
+//ini fungsi untuk CSS
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+
+  key.target.classList.remove("playing");
+}
+
+key.addEventListener("transitionend", removeTransition);
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
